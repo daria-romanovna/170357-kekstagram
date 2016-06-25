2b3f44a69083fdc521f2fd63c326db3120486b4b
daria-romanovna <gorbunova.daria@gmail.com>
2016-06-26 02:05:41 +0700

    Выполнила основное задание

--- src/pictures.js
+++ src/pictures.js
@@ -4,6 +4,7 @@ var filtersBlock = document.querySelector('.filters');
 var picturesContainer = document.querySelector('.pictures');
 var templateElement = document.querySelector('template');
 var elementToClone;
+var pictures = [];
 
 if ('content' in templateElement) {
   elementToClone = templateElement.content.querySelector('.picture');
@@ -12,6 +13,14 @@ if ('content' in templateElement) {
 }
 
 var IMAGE_LOAD_TIMEOUT = 10000;
+
+var Filter = {
+  'ALL': 'filter-popular',
+  'NEW': 'filter-new',
+  'DISCUSSED': 'filter-discussed'
+};
+
+var DEFAULT_FILTER = Filter.ALL;
 
 function hidefiltersBlock() {
   if (!filtersBlock.classList.contains('hidden')) {
@@ -48,13 +57,106 @@ var getPicturesElement = function(data, container) {
   var pictureLoadTimeout = setTimeout(function() {
     pictureImage.src = '';
     element.classList.add('picture-load-failure');
-    hidefiltersBlock();
   }, IMAGE_LOAD_TIMEOUT);
 
 };
 
 hidefiltersBlock();
-window.pictures.forEach(function(picture) {
-  getPicturesElement(picture, picturesContainer);
+
+var PICTURES_LOAD_URL = '//o0.github.io/assets/json/pictures.json'
+
+var getPictures = function(callback) {
+  picturesContainer.classList.add('pictures-loading');
+
+  var xhr = new XMLHttpRequest();
+
+  xhr.onload = function(evt) {
+    picturesContainer.classList.remove('pictures-loading');
+    var loadedData = JSON.parse(evt.target.response);
+    callback(loadedData);
+  };
+
+  xhr.onerror = function(evt) {
+    picturesContainer.classList.remove('pictures-loading');
+    picturesContainer.classList.add('pictures-failure');
+  };
+
+  xhr.timeout = 5000;
+
+  xhr.ontimeout = function(evt) {
+    picturesContainer.classList.remove('pictures-loading');
+    picturesContainer.classList.add('pictures-failure');
+  };
+
+  xhr.open('GET', PICTURES_LOAD_URL);
+  xhr.send();
+};
+
+var renderPictures = function(picturesToRender) {
+  picturesContainer.innerHTML = '';
+  picturesToRender.forEach(function(picture) {
+    getPicturesElement(picture, picturesContainer);
+  });
+}
+
+var getFilteredPictures = function(filter) {
+  var picturesToFilter = pictures.slice(0);
+
+  switch (filter) {
+    case Filter.DISCUSSED:
+      picturesToFilter.sort(function(a, b) {
+        return b.comments - a.comments;
+      });
+      break;
+
+    case Filter.NEW:
+      var currentDate = new Date();
+      var daysAgo = 4;
+      var fourDaysAgo = new Date(currentDate.getTime() - (daysAgo * 24 * 60 * 60 * 1000));
+
+      var newPictures = picturesToFilter.filter(function(picture, i, arr) {
+        var pictureDate = new Date(picture.date);
+        var shouldRemainInArray = pictureDate > fourDaysAgo;
+        return shouldRemainInArray;
+      });
+
+      newPictures.sort(function(a, b) {
+        return new Date(b.date) - new Date(a.date);
+      });
+
+      if (newPictures.length === 0) {
+        picturesContainer.classList.add('pictures-null');
+      }
+
+      return newPictures;
+      break;
+  }
+
+  return picturesToFilter;
+};
+
+var setFilterEnabled = function(filter) {
+  var filteredPictures = getFilteredPictures(filter);
+  renderPictures(filteredPictures);
+};
+
+var setFiltrationEnabled = function() {
+  var filters = filtersBlock.querySelectorAll('.filters-radio');
+  for (var i = 0; i < filters.length; i++) {
+    filters[i].onclick = function(evt) {
+      picturesContainer.classList.remove('pictures-null');
+      setFilterEnabled(this.id);
+    };
+  }
+};
+
+getPictures(function(loadedPictures) {
+  pictures = loadedPictures;
   showfiltersBlock();
+  setFilterEnabled(DEFAULT_FILTER);
+  setFiltrationEnabled();
 });
+
+
+
+
